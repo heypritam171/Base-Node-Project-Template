a21f28763be959962e9bce5c03acc3936698d09f
Pritam Nandi <pritamn016@gmail.com>
2026-04-14 03:41:02 +0530

    Added new migration that can add a constraint in a column of Airport table

---
+++ src/migrations/20260413201318-update-city-airport-association.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airports', {
+      type: 'foreign key',
+      fields: ['cityId'],
+      name: 'city_fkey_constraint',
+      references: {
+        table: 'Cities',
+        field: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint(
+      'Airports',
+      'city_fkey_constraint'
+    );
+  }
+};
